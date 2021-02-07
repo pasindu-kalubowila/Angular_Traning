92d1ac9690d183caaae82e94b42010397b5fe763
Pasindu.Kalubowila <pasindu66deshan@gmail.com>
2021-02-07 21:37:41 +0530

    Start

--- Property-Binding/src/app/app.component.ts
+++ Property-Binding/src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+            <h2>welcome {{name}}</h2>
+            <input [id]="myId" type="text" value="Enter Value">
+            <br><br>
+            <input id= "{{myId}}" type="text" value="Enter Value">
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'Property-Binding';
+  public name = "pasindu";
+  public myId = "testId";
 }
